bd2e6ce0542e55103464fa3c2d069bd072f0f71a
Alan Reche <alanaxelreche@hotmail.com>
2023-03-15 17:01:49 -0300

    Modal cleaning and exported + assets folder

--- src/components/BasicConcepts.js
+++ src/components/BasicConcepts.js
@@ -31,37 +31,8 @@ const BasicConcepts = () => {
       </button>
       <Modal isOpen={isOpen} closeModal={closeModal}>
         <div className="ExplinationModal">
-          <h3>About CRUD App</h3>
-          <p>This is a project that applies the 4 operations in a CRUD:</p>
-          <p style={{ textAlign: "center" }}>
-            <b>Create - Read - Update - Delete</b>
-          </p>
-          <p>
-            The data is not stored and is going to be reseted to an initial
-            state after a page reload, so feel free to add whatever you want.
-          </p>
-          <hr />
-          <p>
-            This project has a total of three components, all with "CRUD" as a
-            prefix to make it ease for others to undertand the source code.
-          </p>
-          <ul>
-            <li>
-              CrudApp: This component holds 3 arrow functions that holds the
-              CRUD logic with 3 states to control the data, the edition of data
-              and the Modal.
-            </li>
-            <li>
-              CrudForm: A form from witch you are able to add the name and type
-              of the pokemon. You can find here the logic to send the
-              information to the parent component and the reset of the entire
-              form.
-            </li>
-            <li>
-              CrudTable: It's a simple table made to display the data and two
-              buttons that will edit or delete the row.
-            </li>
-          </ul>
+          <h3>What is React Router?</h3>
+          
         </div>
       </Modal>
       <Router>
